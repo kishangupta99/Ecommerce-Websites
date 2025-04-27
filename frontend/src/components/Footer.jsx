c66b7f839ac15f7cb6643880c71cddbfe16d277b
kishangupta99 <kishankumar@gmail.com>
2025-04-28 00:53:31 +0530

    project completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+//import React from 'react'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            Forever is a legacy of excellence, crafting premium products/services since 2005. Rooted in innovation and timeless design, we blend artistry with quality to inspire confidence. Every piece reflects our passion for detail and commitment to sustainability. Join millions who trust Forever—where style meets purpose, and every purchase tells a story.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <Link to='tel:91-8318627869'>91-8318627869</Link>
+                <Link to='mailto:acpkishankumar@gmail.com'>acpkishankumar@gmail.com</Link>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
